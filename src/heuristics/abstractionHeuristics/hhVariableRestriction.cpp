3cd7787faba2c6c1ae04267150e156a0a6796433
max-94 <maxborowiec@yahoo.de>
2024-01-03 12:49:12 +0100

    Implemented processing mutex groups.

--- src/heuristics/abstractionHeuristics/hhVariableRestriction.cpp
+++ src/heuristics/abstractionHeuristics/hhVariableRestriction.cpp
@@ -3,23 +3,57 @@
 namespace progression {
 
 hhVariableRestriction::hhVariableRestriction(Model *htn, int index) : Heuristic(htn, index) {
-    pattern = {0,3}; // TODO: Replace with pattern selection procedure.
+    // TODO: Sort pattern ascending. Makes some computations easier.
+    // TODO: Replace with pattern selection procedure.
+    pattern = {0,3};
     restrictedModel = new Model(true, mtrACTIONS, true, true);
+
+    // TODO: Prüfen, ob noch weitere Properties gesetzt werden bevor die read Methoden aufgerufen werden.
 
     // Assuming that variables in pattern do exist! Should be verified to prevent errors...
     // TODO: Simulate reading problem from readClassical and readHierarchy. Computation
     //       beyond reading can be copied from these methods.
 
+    // We have to keep the mapping from original variable id to new id in restricted model.
+    // Required to compute action's precondition, add and delete lists.
+    map<int, int> factOrigToRestrictedMapping;
+    map<int, int> mutexOrigToRestrictedMapping;
+
     // Prepare variables and its names.
-    restrictedModel->numStateBits = pattern.size();
+    restrictedModel->numStateBits = static_cast<int>(pattern.size());
     restrictedModel->factStrs = new string[restrictedModel->numStateBits];
     for (int i = 0; i < restrictedModel->numStateBits; i++) {
+        factOrigToRestrictedMapping[pattern[i]] = i;
         restrictedModel->factStrs[i] = htn->factStrs[pattern[i]];
     }
 
     // Prepare mutex groups.
-    // TODO: Is this considered in our use case? Static conversion: Each state bit represents one mutex group.
-    //       Ansonsten müssten wir über 'varOfStateBit' gehen und die Gruppe extrahieren zu dem betrachteten Pattern.
+    // TODO: Ist dieser Abschnitt überhaupt relevant für uns? Oder könnte man den Teil auch einfach weglassen?
+    map<int, vector<int>> groupToFactMapping;
+    for (int i = 0; i < restrictedModel->numStateBits; i++) {
+        int mutexGroup = htn->varOfStateBit[pattern[i]];
+        groupToFactMapping[mutexGroup].push_back(i); // Hier wird orig mutex id mit neuer Id vermischt!
+    }
+    restrictedModel->numVars = static_cast<int>(groupToFactMapping.size());
+    restrictedModel->firstIndex = new int[restrictedModel->numVars];
+    restrictedModel->lastIndex = new int[restrictedModel->numVars];
+    restrictedModel->varNames = new string[restrictedModel->numVars];
+    restrictedModel->varOfStateBit = new int[restrictedModel->numStateBits];
+
+    int counter = 0;
+    auto it = groupToFactMapping.begin();
+    while (it != groupToFactMapping.end()) {
+        mutexOrigToRestrictedMapping[it->first] = counter;
+        restrictedModel->firstIndex[counter] = it->second.front();
+        restrictedModel->lastIndex[counter] = it->second.back();
+        restrictedModel->varNames[counter] = htn->varNames[it->first];
+        for (int j = restrictedModel->firstIndex[counter]; j <= restrictedModel->lastIndex[counter]; j++) {
+            restrictedModel->varOfStateBit[j] = counter;
+        }
+        it++;
+        counter++;
+    }
+    // TODO: Überlegen wie man mit den strikten Mutexes umgeht.
 
     // TODO: Prepare actions including precs, add, del. We also have to consider conditional effects.
 
